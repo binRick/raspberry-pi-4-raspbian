280c8045f8e1794920b98205ba4da91dae111cfc
binRick <you@example.com>
2023-06-29 20:54:14 -0400

    m

--- utils/packages.sh
+++ utils/packages.sh
@@ -3,6 +3,7 @@ set -eou pipefail
 set -x
 sudo apt -y install  vim cmake  libturbojpeg0-dev libexif-dev libavutil-dev libswscale-dev libgraphicsmagick++1-dev \
 	libavcodec-dev libavformat-dev libavdevice-dev \
-	zsh kitty python3-opencv
+	zsh kitty python3-opencv \
+	imagemagick
 
 sudo apt -y autoremove
