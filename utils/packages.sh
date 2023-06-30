d046bd1d2417484429b55060b13d22d5044ad68b
Rick <you@example.com>
2023-06-29 20:06:01 -0400

    add packages script

--- utils/packages.sh
+++ utils/packages.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eou pipefail
 set -x
-sudo apt -y install  vim cmake  libturbojpeg0-dev libexif-dev libavutil-dev libswscale-dev libgraphicsmagick++1-dev
+sudo apt -y install  vim cmake  libturbojpeg0-dev libexif-dev libavutil-dev libswscale-dev libgraphicsmagick++1-dev \
+	libavcodec-dev libavformat-dev libavdevice-dev
