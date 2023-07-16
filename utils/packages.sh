3242e2463b9093c11095bde3f7e985837f446472
Your Name <you@example.com>
2023-07-16 11:26:11 -0700

    add espeak

--- utils/packages.sh
+++ utils/packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eou pipefail
 set -x
-sudo apt -y install  vim cmake  libturbojpeg0-dev libexif-dev libavutil-dev libswscale-dev libgraphicsmagick++1-dev \
+sudo apt -y install espeak vim cmake  libturbojpeg0-dev libexif-dev libavutil-dev libswscale-dev libgraphicsmagick++1-dev \
 	libavcodec-dev libavformat-dev libavdevice-dev \
 	zsh kitty python3-opencv \
 	imagemagick  libortp-dev libortp15      libatlas-base-dev lynx sysstat npm nodejs \
