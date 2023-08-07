ea353562d4032438ca2e43819bffc087eb4ae12c
Your Name <you@example.com>
2023-08-08 05:55:53 +0800

    tweak for orange pi

--- camera/ffmpeg-sdl.sh
+++ camera/ffmpeg-sdl.sh
@@ -1,1 +1,1 @@
-ffmpeg -i /dev/video0 -c:v rawvideo -pix_fmt yuv420p -f sdl "SDL output"
+ffmpeg -i /dev/video0 -c:v rawvideo -pix_fmt yuv420p -f sdl "Realtime Video Stream"
--- utils/packages.sh
+++ utils/packages.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -eou pipefail
 set -x
-sudo apt -y install espeak vim cmake  libturbojpeg0-dev libexif-dev libavutil-dev libswscale-dev libgraphicsmagick++1-dev \
-	libavcodec-dev libavformat-dev libavdevice-dev \
+sudo apt -y install espeak vim cmake  libturbojpeg0-dev  \
 	zsh kitty python3-opencv \
 	imagemagick  libortp-dev libortp15      libatlas-base-dev lynx sysstat npm nodejs \
-	libncurses-dev ninja-build gfortran docker.io libgif-dev libgif7 raspberrypi-kernel-headers \
-	v4l-utils python3-usb libgtk-3-dev libwebkit2gtk-4.0-dev
 
 sudo apt -y autoremove
