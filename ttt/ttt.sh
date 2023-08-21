dd3629a6a865b4f2d307f007048854b9e74830ec
rick <rick@ricks-MBP.lan>
2023-08-20 21:11:14 -0400

    near game state extraction

--- ttt/ttt.sh
+++ ttt/ttt.sh
@@ -27,6 +27,16 @@ I=images/board2.jpg	#  OX
 
 #I=images/board1.jpg 	# empty board
 
+#I=images/1584175590822.png
+#I=images/tic-tac-toe-icon-illustration-free-vector.jpeg
+#I=images/ex.jpeg
+#I=images/t5.jpg
+#I=images/t6.jpg
+#I=images/thumbnail.png
+#I=images/tic-tac-toe-2362786.jpeg
+#I=images/tic-tac-toe-game-illustration-38972778.jpeg
+#I=images/tic-tac-toe-game-vector-hand-drawn-competition-on-a-paper-background-2AH4FRR.jpeg
+#I=images/depositphotos_72683411-stock-illustration-hand-drawn-tic-tac-toe.jpeg
 #I=images/t1.jpg
 #I=images/t2.jpg
 #I=images/t3.jpg
