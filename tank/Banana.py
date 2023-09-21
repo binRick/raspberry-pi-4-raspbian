55060166b3f4e12e18627937d83b1d23e42fb236
Your Name <you@example.com>
2023-09-21 22:52:35 +0100

    functional webcam banana detector

--- tank/Banana.py
+++ tank/Banana.py
@@ -5,138 +5,78 @@ from pycoral.adapters import common
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 
-vid = cv2.VideoCapture(0)
-'''
-        ret, frame = vid.read()
-        image = cv2.resize(frame, (320,240))
-        print(f'processed frame #{qty}')
-        qty = qty + 1
-'''
-
-def find_bananas(args, interpreter, images, labels):
-    qty = 0
-    size = common.input_size(interpreter)
-    for index, img in enumerate(images):
-        if not os.path.isfile(img):
-            continue
-        print(f'processing {img}')
-        image = Image.open(img).convert('RGB').resize(size, Image.LANCZOS)
-
-        params = common.input_details(interpreter, 'quantization_parameters')
+class BananaDetector(object):
+    labels = None
+    interpreter = None
+    size = None
+    input_mean = 128.0
+    input_std = 128.0
+    count = 5
+    top_k = 1
+    threshold = 0.0
+    is_banana_threshold = 0.20
+    _labels = 'labels/mobilenet_v1_0.75_192_quant_edgetpu.txt'
+    _model = 'models/mobilenet_v1_0.75_192_quant_edgetpu.tflite'
+    def __init__(self):
+        print('banana detector setup')
+        self.labels = read_label_file(self._labels) if self._labels else {}
+        self.interpreter = make_interpreter(*self._model.split('@'))
+        self.interpreter.allocate_tensors()
+        if common.input_details(self.interpreter, 'dtype') != np.uint8:
+          raise ValueError('Only support uint8 input type.')
+        self.size = common.input_size(self.interpreter)
+    def is_banana_file(self, FILE):
+        if not os.path.isfile(FILE):
+            return False
+        image = Image.open(FILE).convert('RGB').resize(self.size, Image.LANCZOS)
+        params = common.input_details(self.interpreter, 'quantization_parameters')
         scale = params['scales']
         zero_point = params['zero_points']
-        mean = args.input_mean
-        std = args.input_std
-        if abs(scale * std - 1) < 1e-5 and abs(mean - zero_point) < 1e-5:
-            common.set_input(interpreter, image)
+        if abs(scale * self.input_std - 1) < 1e-5 and abs(self.input_mean - zero_point) < 1e-5:
+            common.set_input(self.interpreter, image)
         else:
-            normalized_input = (np.asarray(image) - mean) / (std * scale) + zero_point
+            normalized_input = (np.asarray(image) - self.input_mean) / (input_std * scale) + zero_point
             np.clip(normalized_input, 0, 255, out=normalized_input)
-            common.set_input(interpreter, normalized_input.astype(np.uint8))
+            common.set_input(self.interpreter, normalized_input.astype(np.uint8))
 
-        print('----INFERENCE TIME----')
-        if index == 0:
-              print('Note: The first inference on Edge TPU is slow because it includes',
-                    'loading the model into Edge TPU memory.')
-        for _ in range(args.count):
+        for _ in range(self.count):
             start = time.perf_counter()
-            interpreter.invoke()
+            self.interpreter.invoke()
             inference_time = time.perf_counter() - start
-            classes = classify.get_classes(interpreter, args.top_k, args.threshold)
+            classes = classify.get_classes(self.interpreter, self.top_k, self.threshold)
             print('%.1fms' % (inference_time * 1000))
 
         print('-------RESULTS--------')
         for c in classes:
-            print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+            print('%s: %.5f' % (self.labels.get(c.id, c.id), c.score))
+            if c.score >= self.is_banana_threshold:
+                return True
+        return False
 
 
-def main():
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument(
-      '-m', '--model', required=True, help='File path of .tflite file.')
-  parser.add_argument(
-      '-i', '--input', required=True, help='Image to be classified.')
-  parser.add_argument(
-      '-l', '--labels', help='File path of labels file.')
-  parser.add_argument(
-      '-k', '--top_k', type=int, default=1,
-      help='Max number of classification results')
-  parser.add_argument(
-      '-t', '--threshold', type=float, default=0.0,
-      help='Classification score threshold')
-  parser.add_argument(
-      '-c', '--count', type=int, default=5,
-      help='Number of times to run inference')
-  parser.add_argument(
-      '-a', '--input_mean', type=float, default=128.0,
-      help='Mean value for input normalization')
-  parser.add_argument(
-      '-s', '--input_std', type=float, default=128.0,
-      help='STD value for input normalization')
-  args = parser.parse_args()
-
-  labels = read_label_file(args.labels) if args.labels else {}
-
-  interpreter = make_interpreter(*args.model.split('@'))
-  interpreter.allocate_tensors()
-
-  # Model must be uint8 quantized
-  if common.input_details(interpreter, 'dtype') != np.uint8:
-    raise ValueError('Only support uint8 input type.')
-
-  size = common.input_size(interpreter)
-  images = []
-  if ',' in args.input:
-      images = args.input.split(',')
-  else:
-      images.append(args.input)
-  print(f'images: {images}')
-
-  find_bananas(args, interpreter, images, labels)
-  '''
-
-  images = []
-  if ',' in args.input:
-      images = args.input.split(',')
-  else:
-      images.append(args.input)
-
-  for index, img in enumerate(images):
-      if not os.path.isfile(img):
-          continue
-      print(f'\nAnalzying image {img}')
-
-      image = Image.open(img).convert('RGB').resize(size, Image.LANCZOS)
-
-      params = common.input_details(interpreter, 'quantization_parameters')
-      scale = params['scales']
-      zero_point = params['zero_points']
-      mean = args.input_mean
-      std = args.input_std
-      if abs(scale * std - 1) < 1e-5 and abs(mean - zero_point) < 1e-5:
-        common.set_input(interpreter, image)
-      else:
-        normalized_input = (np.asarray(image) - mean) / (std * scale) + zero_point
-        np.clip(normalized_input, 0, 255, out=normalized_input)
-        common.set_input(interpreter, normalized_input.astype(np.uint8))
-
-      print('----INFERENCE TIME----')
-      if index == 0:
-          print('Note: The first inference on Edge TPU is slow because it includes',
-                'loading the model into Edge TPU memory.')
-      for _ in range(args.count):
-        start = time.perf_counter()
-        interpreter.invoke()
-        inference_time = time.perf_counter() - start
-        classes = classify.get_classes(interpreter, args.top_k, args.threshold)
-        print('%.1fms' % (inference_time * 1000))
-
-      print('-------RESULTS--------')
-      for c in classes:
-        print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
-  '''
+def find_webcam_bananas(bd):
+    vid = cv2.VideoCapture(0)
+    qty = 0
+    while True:
+        ret, frame = vid.read()
+        image = cv2.resize(frame, (320,240))
+        tf = './.tmp.png'
+        cv2.imwrite(tf, image)
+        is_banana = bd.is_banana_file(tf)
+        print(f'{tf} is banana?   {is_banana}')
+        print(f'processed frame #{qty}')
+        time.sleep(1.0)
+        qty = qty + 1
 
 
 if __name__ == '__main__':
-  main()
+  bd = BananaDetector()
+  images = [
+          'images/bananas/banana1.jpg',
+          'images/bananas/banana2.jpg',
+  ]
+  for i in images:
+      is_banana = bd.is_banana_file(i)
+      print(f'{i} is banana?   {is_banana}')
+
+  find_webcam_bananas(bd)
--- tank/test-banana.sh
+++ tank/test-banana.sh
@@ -6,12 +6,8 @@ if [[ ! -d .v ]]; then
 	source .v/bin/activate
 	pip -q install numpy Pillow numpy opencv-python
 	python3 -m pip install --extra-index-url https://google-coral.github.io/py-repo/ pycoral~=2.0
-	#python3 -m pip install --extra-index-url https://google-coral.github.io/py-repo/ pycoral~=2.0
 fi
 source .v/bin/activate
-cmd="time python3 ./Banana.py \
-	--model models/mobilenet_v1_0.75_192_quant_edgetpu.tflite \
-	--labels labels/mobilenet_v1_0.75_192_quant_edgetpu.txt \
-	--input $BANANAS"
-	echo -e "$cmd" >&2
-	eval "$cmd"
+cmd="time python3 ./Banana.py"
+echo -e "$cmd" >&2
+eval "$cmd"
